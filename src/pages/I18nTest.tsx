b7fdd1a3e56c367ddf3296d8c24eb94d9ebbdaf5
guiuiux <gferreira.uiux@gmail.com>
2025-06-14 14:19:22 -0300

    added SF Pro Text Font,

--- src/pages/I18nTest.tsx
+++ src/pages/I18nTest.tsx
@@ -1,12 +1,17 @@
 import { Button } from "@/components/ui/button";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 
 export default function I18nTest() {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-4">
-      <h1 className="text-2xl font-bold">{t("welcome")}</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-4 p-16">
+      <h1 className="text-5xl font-regular tracking-tighter font-sf text-zinc-500">
+        <Trans
+          i18nKey="welcome.title"
+          components={{ bold: <span className="font-bold  text-zinc-800" /> }}
+        />
+      </h1>
       <div className="flex gap-2">
         <Button
           variant={i18n.resolvedLanguage === "en" ? "default" : "outline"}
@@ -21,7 +26,7 @@ export default function I18nTest() {
           PT-BR
         </Button>
       </div>
-      <p>{t("change_language")}</p>
+      <p>{i18n.t("change_language")}</p>
     </div>
   );
 }
